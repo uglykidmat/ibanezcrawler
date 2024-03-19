13847b50d44e62f1a51ad96d2479151654317258
uglykidmat <mathieu.peyrot.47@gmail.com>
2024-03-19 10:40:56 +0100

    (feat): add DomCrawler pack

---
+++ src/Crawler/Crawler.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Crawler;
+use Symfony\Component\DomCrawler\Crawler;
+
+$html = <<<'HTML' <!DOCTYPE html>
+    <html>
+
+    <body>
+        <p class="message">Hello World!</p>
+        <p>Hello Crawler!</p>
+    </body>
+
+    </html>
+    HTML;
+
+    $crawler = new Crawler($html);
+
+    foreach ($crawler as $domElement) {
+    var_dump($domElement->nodeName);
+    }
