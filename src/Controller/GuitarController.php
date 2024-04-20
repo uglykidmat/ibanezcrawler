b70776623b28f1bcf766905a856609671a49efe9
uglykidmat <mathieu.peyrot.47@gmail.com>
2024-04-20 20:26:27 +0200

    (feat): guitar controller update, search by family or specific model now

--- src/Controller/GuitarController.php
+++ src/Controller/GuitarController.php
@@ -15,30 +15,60 @@ class GuitarController extends AbstractController
     ) {
     }
 
-    #[Route('/guitar/{family}', name: 'guitar_by_family')]
-    public function getByFamily(string $family): JsonResponse
+    #[Route('/guitars/{family}', name: 'guitars_by_family')]
+    public function getAllGuitarsByFamily(string $family): JsonResponse
     {
         $family = strlen($family) > 3 ? ucfirst($family) : strtoupper($family);
-        $allGuitarsByFamily = $this->entityManager->getRepository(Guitar::class)->findByFamily($family);
+        if ($allGuitarsByFamily = $this->entityManager->getRepository(Guitar::class)->findByFamily($family)) {
+            $response = [];
+            foreach ($allGuitarsByFamily as $guitar) {
+                $response[] = [
+                    'model' => $guitar->getModel(),
+                    'body type' => $guitar->getBodytype(),
+                    'body material' => $guitar->getBodymaterial(),
+                    'neck type' => $guitar->getNecktype(),
+                    'neck joint' => $guitar->getNeckjoint(),
+                    'finishes' => $guitar->getFinishes(),
+                ];
+            }
 
-        $response = [];
-
-        foreach ($allGuitarsByFamily as $guitar) {
-            //dd($guitar);
-            $response[] = [
-                'model' => $guitar->getModel(),
-                'body type' => $guitar->getBodytype(),
-                'body material' => $guitar->getBodymaterial(),
-                'neck type' => $guitar->getNecktype(),
-                'neck joint' => $guitar->getNeckjoint(),
-                'finishes' => $guitar->getFinishes(),
-            ];
+            return $this->json([
+                'info' => 'Success',
+                'data' => $response,
+            ]);
+        } else {
+            return $this->json([
+                'info' => 'Failure',
+                'data' => 'No models found !',
+            ]);
         }
-        //dd($response);
+    }
 
-        return $this->json([
-            'info' => 'Success',
-            'data' => $response,
-        ]);
+    #[Route('/guitar/{model}', name: 'guitar_by_model')]
+    public function getGuitarModel(string $model): JsonResponse
+    {
+        $response = [];
+        if ($guitarByModel = $this->entityManager->getRepository(Guitar::class)->findByModel($model)) {
+            foreach ($guitarByModel as $guitar) {
+                $response[] = [
+                    'model' => $guitar->getModel(),
+                    'body type' => $guitar->getBodytype(),
+                    'body material' => $guitar->getBodymaterial(),
+                    'neck type' => $guitar->getNecktype(),
+                    'neck joint' => $guitar->getNeckjoint(),
+                    'finishes' => $guitar->getFinishes(),
+                ];
+            }
+
+            return $this->json([
+                'info' => 'Success',
+                'data' => $response,
+            ]);
+        } else {
+            return $this->json([
+                'info' => 'Failure',
+                'data' => 'No model found !',
+            ]);
+        }
     }
 }
