b5742ce4434e5799e45d773e006ad36fabfbf28c
uglykidmat <mathieu.peyrot.47@gmail.com>
2024-04-20 19:33:23 +0200

    (feat): guitar controller, outputs guitars list by family for now

--- src/Controller/GuitarController.php
+++ src/Controller/GuitarController.php
@@ -2,18 +2,43 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Guitar;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class GuitarController extends AbstractController
 {
+    public function __construct(
+        public EntityManagerInterface $entityManager
+    ) {
+    }
+
     #[Route('/guitar/{family}', name: 'guitar_by_family')]
-    public function getFamily(): JsonResponse
+    public function getByFamily(string $family): JsonResponse
     {
+        $family = strlen($family) > 3 ? ucfirst($family) : strtoupper($family);
+        $allGuitarsByFamily = $this->entityManager->getRepository(Guitar::class)->findByFamily($family);
+
+        $response = [];
+
+        foreach ($allGuitarsByFamily as $guitar) {
+            //dd($guitar);
+            $response[] = [
+                'model' => $guitar->getModel(),
+                'body type' => $guitar->getBodytype(),
+                'body material' => $guitar->getBodymaterial(),
+                'neck type' => $guitar->getNecktype(),
+                'neck joint' => $guitar->getNeckjoint(),
+                'finishes' => $guitar->getFinishes(),
+            ];
+        }
+        //dd($response);
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/GuitarController.php',
+            'info' => 'Success',
+            'data' => $response,
         ]);
     }
 }
