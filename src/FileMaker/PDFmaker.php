42ff25d2e584b3a63ff9c9ded41d5327bc3a0946
uglykidmat <mathieu.peyrot.47@gmail.com>
2024-08-07 12:43:47 +0200

    (chore): clean hierarchical names + implement first library class call weeee

--- src/FileMaker/PDFmaker.php
+++ src/FileMaker/PDFmaker.php
@@ -2,6 +2,22 @@
 
 namespace App\FileMaker;
 
-class PDFmaker
+//dd(__DIR__);
+//require (__DIR__ . '/FPDF/fpdf.php');
+use App\FileMaker\FPDF\FPDF as FPDF;
+
+class PDFmaker extends FPDF
 {
+    private $FPDFtest;
+
+    public function __construct()
+    {
+        $this->FPDFtest = new FPDF();
+        //$this->FPDFtest = 'new FPDF()';
+    }
+
+    public function log()
+    {
+        dd($this->FPDFtest);
+    }
 }
