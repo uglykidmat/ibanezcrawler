10b95c3aea62673409dfb5daafe8e59cc133a588
uglykidmat <mathieu.peyrot.47@gmail.com>
2024-09-13 13:10:27 +0200

    (fix): add the usual IF NOT EXISTS etc to the migration file

--- migrations/Version20240913110618.php
+++ migrations/Version20240913110618.php
@@ -19,16 +19,16 @@ final class Version20240913110618 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE finish_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE finish (id INT NOT NULL, name VARCHAR(255) NOT NULL, short_name VARCHAR(255) NOT NULL, extra_finish_info VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        // this up() migration is auto-generated, please modify it to your needs 
+        $this->addSql('CREATE SEQUENCE IF NOT EXISTS finish_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE IF NOT EXISTS finish (id INT NOT NULL, name VARCHAR(255) NOT NULL, short_name VARCHAR(255) NOT NULL, extra_finish_info VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE finish_id_seq CASCADE');
-        $this->addSql('DROP TABLE finish');
+        $this->addSql('CREATE SCHEMA IF NOT EXISTS public');
+        $this->addSql('DROP SEQUENCE IF EXISTS finish_id_seq CASCADE');
+        $this->addSql('DROP TABLE IF EXISTS finish');
     }
 }
