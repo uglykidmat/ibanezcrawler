4fb2fe9486999816b495dba5f783168322e5e44b
uglykidmat <mathieu.peyrot.47@gmail.com>
2024-10-21 22:41:35 +0200

    (fix): remove getAllFields output from guitar serializer

--- src/Entity/Guitar.php
+++ src/Entity/Guitar.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Ignore;
 
 #[ORM\Entity(repositoryClass: GuitarRepository::class)]
 class Guitar
@@ -650,6 +651,8 @@ class Guitar
         return $this;
     }
 
+
+    #[Ignore()]
     public function getAllFields()
     {
         return get_object_vars($this);
